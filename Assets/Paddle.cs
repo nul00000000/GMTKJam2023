3c75b207f021067986920b79d8bfa23220959651
espvolt <espvolt@gmail.com>
2023-07-07 19:22:42 -0400

    Wind  Please enter the commit message for your changes. Lines starting

--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -6,12 +6,11 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     // Start is called before the first frame 
-    public Transform transform;
-    public Rigidbody rigidbody;
-    public HingeJoint HingeConstraint;
+    public Rigidbody rigidBody;
+    public HingeJoint hingeConstraint;
     void Start()
     {
-        rigidbody.maxAngularVelocity = Mathf.Infinity;
+        rigidBody.maxAngularVelocity = Mathf.Infinity;
     }
 
     // Update is called once per frame
@@ -21,7 +20,7 @@ public class Paddle : MonoBehaviour
         // Vector3 newAngles = new Vector3(angles.x, angles.y, Math.Clamp(angles.z, -50, 50));
         // transform.rotation = Quaternion.Euler(newAngles);
         if (Input.GetKey(KeyCode.Space)) {
-            rigidbody.AddTorque(new Vector3(0, 0, -40));
+            rigidBody.AddTorque(new Vector3(0, 0, -40));
         }
     }
 }
