a414787ee8d6605cc34e1c1e696d348aa52db75f
nul00000000 <adamschultzer@gmail.com>
2023-07-07 19:28:34 -0400

    spinner

---
+++ Assets/Spinner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour {
+
+    private Rigidbody body;
+
+    public bool left = true;
+
+    void Start() {
+        body = GetComponent<Rigidbody>();
+        body.maxAngularVelocity = 7;
+    }
+
+    void Update() {
+        if(Input.GetKey(KeyCode.W)) {
+            body.AddTorque(new Vector3(0, 0, left ? 10 : -10));
+        }
+    }
+}
