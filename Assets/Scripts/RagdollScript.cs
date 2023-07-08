6f4822fff40aef7df5d99b8659245bfa18efd198
espvolt <espvolt@gmail.com>
2023-07-08 06:54:18 -0400

    fucki

---
+++ Assets/Scripts/RagdollScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class RagdollScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody[] rigidBodies;
+    public Renderer cube;
+    
+    public bool pressed;
+    public float time = 0;
+
+    void Start() {
+        rigidBodies = GetComponentsInChildren<Rigidbody>();
+        DisableRagdoll();
+
+        cube.material.SetColor("_Color", new Color(cube.material.color.r, cube.material.color.g, cube.material.color.b, 0));
+    }
+
+
+    void EnableRagdoll() {
+        foreach (Rigidbody rigidBody in rigidBodies) {
+            rigidBody.isKinematic = false;
+        }
+    }
+
+    void DisableRagdoll() {
+        foreach (Rigidbody rigidBody in rigidBodies) {
+            rigidBody.isKinematic = true;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - time > 3) {
+            SceneManager.LoadScene(1);
+        } else if (Time.time - time > 1) {
+            cube.material.SetColor("_Color", new Color(cube.material.color.r, cube.material.color.g, cube.material.color.b, Mathf.Min(Time.time - time - 1, 204f / 255f)));
+        } 
+    }
+
+    public void OnClick() {
+        pressed = true;
+        time = Time.time;
+        EnableRagdoll();
+    }
+}
---
+++ Assets/Scripts/UiScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public RectTransform buttonTransform;
+    public bool pressed = false;
+    
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (!pressed) {
+            buttonTransform.localScale = new Vector3(.05f * Mathf.Sin(Time.time) + .2f, .05f * Mathf.Sin(Time.time) + .2f, 1);
+        } else {
+            buttonTransform.localScale *= .6f;
+        }
+
+
+    }
+
+    public void OnClick() {
+        pressed = true;
+    }
+
+}
