11cac7e8a22938e942414c86a6eb3fc53a4350c9
nul00000000 <adamschultzer@gmail.com>
2023-07-10 19:50:09 -0400

    fix reset button

--- Assets/Scripts/ResetButton.cs
+++ Assets/Scripts/ResetButton.cs
@@ -1,27 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ResetButton : MonoBehaviour
 {
-
-    public GameObject recreate;
-    public LockRotation mainCamera;
-
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
         if(Input.GetKeyDown(KeyCode.R)) {
-            GameObject obj = Instantiate(recreate, new Vector3(0, 1.36f, 0), new Quaternion());
-            obj.GetComponent<ResetButton>().mainCamera = mainCamera;
-            mainCamera.follow = obj.GetComponentInChildren<Rigidbody>();
-            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
