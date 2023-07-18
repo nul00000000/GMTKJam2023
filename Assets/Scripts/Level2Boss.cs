68271556d552a4b90a70108a7c8252fc99c0527d
nul00000000 <adamschultzer@gmail.com>
2023-07-18 03:24:27 -0400

    actual starting boss 2

--- Assets/Scripts/Level2Boss.cs
+++ Assets/Scripts/Level2Boss.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class Level2Boss : MonoBehaviour {
 
+    public GameObject bossFight;
+
+    private bool running = false;
+
     void Start() {
-        
+        bossFight.SetActive(false);
     }
 
     void OnTriggerEnter(Collider collision) {
         if (collision.gameObject.tag == "Player") {
-            
+            running = true;
+            bossFight.SetActive(true);
         }
     }
 
     void OnTriggerExit(Collider collision) {
         if (collision.gameObject.tag == "Player") {
-            
+            running = false;
+            bossFight.SetActive(false);
         }
     }
 
